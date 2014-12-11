cd9e5289e626360474268aab8d43fdced011f394
zenelg <zenel.gomez@gmail.com>
2014-12-11 15:45:33 -0800

    First Pass
    ProgrammingAssigment2 -
    cacheSolve - First Pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R function is able to cache potentially time-consuming computations.
+# take advantage of the scoping rules of the R language and how they can 
+# be manipulated to preserve state inside of an R object.
 
-## Write a short comment describing this function
+# makeVector creates a special "vector", which is really a list containing a function to
+# set the value of the vector, get the value of the vector
+# set the value of the mean and the value of the mean
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse  # calculate inverse here via solve ?
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the Inverse of the special "matrix" 
+# created with the above function. 
+# However, it first checks to see if the Inverse has already been calculated. 
+# If so, it gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the data and sets the value of the 
+# Inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...) # calculate inverse via solve
+    x$setinverse(m)
+    m
 }
