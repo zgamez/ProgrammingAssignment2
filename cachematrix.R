493c48a917057155c031486ecf91acd8c43dfebd
zenelg <zenelg@users.noreply.github.com>
2014-12-13 08:09:23 -0800

    ProgrammingAssigment2 -
    cacheSolve - Second Pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,14 @@
-# R function is able to cache potentially time-consuming computations.
-# take advantage of the scoping rules of the R language and how they can 
-# be manipulated to preserve state inside of an R object.
-
-# makeVector creates a special "vector", which is really a list containing a function to
-# set the value of the vector, get the value of the vector
-# set the value of the mean and the value of the mean
-
+# Creates a special "matrix"to set the value of the matrix, 
+# get the value of the matrix and set the value of the inverse
 
 makeCacheMatrix <- function(x = numeric()) {
     m <- NULL
     set <- function(y) {
-        x <<- y
-        m <<- NULL
+        x <<- y                         # x in the containing environment is initialized
+        m <<- NULL                      # m in the containing environment is initialized
     }
     get <- function() x
-    setinverse <- function(inverse) m <<- inverse  # calculate inverse here via solve ?
+    setinverse <- function(inverse) m <<- inverse  
     getinverse <- function() m
     list(set = set, get = get,
          setinverse = setinverse,
@@ -22,21 +16,27 @@ makeCacheMatrix <- function(x = numeric()) {
 }
 
 
-# The following function calculates the Inverse of the special "matrix" 
+# cachesolve calculates the Inverse of the special "matrix" 
 # created with the above function. 
-# However, it first checks to see if the Inverse has already been calculated. 
-# If so, it gets the inverse from the cache and skips the computation. 
-# Otherwise, it calculates the inverse of the data and sets the value of the 
-# Inverse in the cache via the setinverse function.
+
 
 cacheSolve <- function(x, ...) {
+    
+    # Checks to see if the Inverse has already been calculated. 
+    # If so, it gets the inverse from the cache and skips the computation. 
+        
     m <- x$getinverse()
     if(!is.null(m)) {
-        message("getting cached data")
+        message("getting cached data")  # since local variable 'm' exists within global there is no need to search the containing
         return(m)
     }
+    
+    # Calculates the inverse of the data and sets the value of the 
+    # Inverse in the cache via the setinverse function.
+    
     data <- x$get()
-    m <- solve(data, ...) # calculate inverse via solve
+    m <- solve(data, ...)               # calculate inverse via solve
     x$setinverse(m)
     m
 }
+  
